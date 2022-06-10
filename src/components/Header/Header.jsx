502aa4ad1a14ae1ab3073e0eedcc2187e76c634e
21ciprian <2021ciprianoffice@gmail.com>
2022-06-10 10:26:21 +0100

    Added Title in the Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,14 @@
+import styles from './Header.module.css'
 function Header() {
-	return <div>Header</div>
+	return (
+		<header className={styles.header}>
+			<h3>Where in the world?</h3>
+			<div className={styles.toggleContainer}>
+				<p>icon</p>
+				<p>Dark mode</p>
+			</div>
+		</header>
+	)
 }
 
 export default Header
