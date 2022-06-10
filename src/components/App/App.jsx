a694261840f3d791e6b7b475050fd16e24310a85
Dovydas Lozys <755086+reduktorius@users.noreply.github.com>
2022-06-10 12:59:12 +0100

    imports `Navbar`

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
 
 import Card from '../Card/Card';
-import Header from '../Header/Header'
+import Header from '../Header/Header';
+import Navbar from '../Navbar/Navbar';
 
-import style from './App.module.css'
+import style from './App.module.css';
 
 function App() {
 	const [allCountries, setAllCountries] = useState(null);
@@ -24,11 +25,11 @@ function App() {
 	return (
 		<div>
 			<Header />
+			<Navbar />
 			<main className={style.container}>
-			{allCountries.map((country) => {
-				return <Card key={country.name.common} props={country}/>
-			})
-			}
+				{allCountries.map((country) => {
+					return <Card key={country.name.common} props={country}/>
+				})}
 			</main>
 		</div>
 	)
