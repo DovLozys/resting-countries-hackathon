c445b3d379f9fc96052283236aaa769f424a6ffd
Dovydas Lozys <755086+reduktorius@users.noreply.github.com>
2022-06-10 11:27:42 +0100

    adds basic structure to `Card` component

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,6 +1,22 @@
 import styles from './Card.module.css'
-function Card() {
-	return <article className={styles.card}>Card</article>
+
+function Card(props) {
+	return (
+		// what we will want displayed:
+		// flag image, png format
+		// capital name
+		// region name
+		// population
+
+		//TODO: fix the props.id
+		<article className={styles.card} key={props.name.common}>
+			<img src={props.flags.png} alt={props.flag} />
+			<h2>{props.name.common}</h2>
+			<p>Region: {props.region}</p>
+			<p>Capital: {props.capital[0]}</p>
+			<p>Population: {props.population}</p>
+		</article>
+	);
 }
 
-export default Card
+export default Card;
