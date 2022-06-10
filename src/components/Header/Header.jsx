1f959c8608f0a0f1d10a3c59309f12c3d5089c61
21ciprian <2021ciprianoffice@gmail.com>
2022-06-10 10:40:51 +0100

    Added FontAwesome icons for light and dark mode

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,13 @@
+import {faMoon, faSun} from '@fortawesome/free-solid-svg-icons'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import styles from './Header.module.css'
 function Header() {
 	return (
 		<header className={styles.header}>
 			<h3>Where in the world?</h3>
 			<div className={styles.toggleContainer}>
-				<p>icon</p>
+				<FontAwesomeIcon icon={faMoon} />
+				<FontAwesomeIcon icon={faSun} />
 				<p>Dark mode</p>
 			</div>
 		</header>
