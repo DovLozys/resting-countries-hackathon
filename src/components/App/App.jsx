436d25d6009d378eb3646901cbf3e283cc751bf7
Dovydas Lozys <755086+reduktorius@users.noreply.github.com>
2022-06-10 10:48:43 +0100

    adds fetching of all countries from api

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,32 @@
+import React, { useState, useEffect } from 'react';
+
 import Header from '../Header/Header'
+
 import './App.css'
 
 function App() {
+	const [allCountries, setAllCountries] = useState(null);
+
+	useEffect(() => {
+		async function fetchData() {
+			const res = await fetch('https://restcountries.com/v3.1/all');
+			const data = await res.json();
+			setAllCountries(data);
+		}
+		fetchData();
+	}, [])
+
+	if (!allCountries) {
+		return <div>Loading...</div>
+	}
+
 	return (
 		<div>
 			Hello World!
 			<Header />
+			{allCountries[0].name.common}
 		</div>
 	)
 }
 
-export default App
+export default App;
